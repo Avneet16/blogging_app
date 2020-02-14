fb9411d8f3689f259ed8cf94ccfa0f23874aa92b
Avneet <ramarora27@gail.com>
2020-02-15 03:22:08 +0530

    add logout functionality

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView
+
 urlpatterns=[
     path('',views.post_list,name='post_list'),
     path('post/<int:pk>',views.post_detail,name='post_detail'),
@@ -8,5 +8,4 @@ urlpatterns=[
     path('post/edit/<int:pk>',views.post_edit,name='post_edit'),
     path('drafts/',views.post_draft,name='post_draft'),
     path('post/publish/<int:pk>',views.post_publish,name='post_publish'),
-    path('accounts/login',LoginView.as_view(template_name='registration/login.html'),name='login'),
 ]
